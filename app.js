4ec0488d7acaee0cf97084e41a8e405958559fed
Andre Barber <andre.barber0505@gmail.com>
2022-04-28 13:30:50 -0500

    start

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port: ${PORT}`);
+});
