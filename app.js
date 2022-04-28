eb1628b3f2e41a34d7cd8715c334ddf2377a4697
Tim Brandau <96590473+tacticalbison@users.noreply.github.com>
2022-04-28 16:26:07 -0500

    completed exercise through part two

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const { db, Page, Users, User } = require("./models");
 
 const app = express();
 
@@ -7,12 +8,24 @@ app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port: ${PORT}`);
-});
+const init = async (req, res) => {
+  // await Page.sync();
+  // await User.sync();
+  await db.sync({ force: true }); // { force: true } <-- Optional if we need to overwrite tables.
+
+  app.listen(PORT, () => {
+    console.log(`App listening in port: ${PORT}`);
+  });
+};
+
+init();
